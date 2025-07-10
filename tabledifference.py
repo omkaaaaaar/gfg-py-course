0530b2473f805a8810cae8b9c8f1c9c921d375cd
omkaaaaaar <omkarpatkarr@gmail.com>
2025-07-11 02:55:26 +0530

    k

---
+++ tabledifference.py
@@ -0,0 +1,8 @@
+#User function Template for python3
+n1 = int(input("Enter first number: "))
+n2 = int(input("Enter second number: "))
+print("Difference (of both & its) Table:")
+# Your code here
+n=n1-n2
+r=range(n,n*11,n)
+print(*r)
---
+++ whileloop.py
@@ -0,0 +1,9 @@
+#User function Template for python3
+x = int(input("Enter Your Number: "))
+
+# Your code here
+n=x
+print(n, end=' ')
+while n>0:
+    print(n-1, end=' ')
+    n=n-1
