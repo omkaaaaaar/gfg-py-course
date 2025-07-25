a86800bacc5bab62afec91628bcca9fae868af4d
omkaaaaaar <omkarpatkarr@gmail.com>
2025-07-25 21:34:56 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -10,9 +10,10 @@ printDetails(102, "Mobile")
 print()
 printDetails(103)  # Only ID provided, others will be default "NA"
 print()
+print("----------------------------")
+print()
 
-
-#Variable Length Arguments
+#Variable Length Arguments Tuple Based, Only values
 def sum(*elements):
     res=0
     for x in elements:
@@ -23,3 +24,17 @@ print(sum(10,20))
 print(sum(10,20,30))
 print(sum(10))
 print(sum())
+print()
+print("----------------------------")
+print()
+
+#Function with Default and Variable Length Arguments - Dictionary Based, Key-Value Pairs
+def printDetails(**details):
+    for key, value in details.items(): # unpacking the dictionary, basically items that are stored in details
+        print(f"{key} is {value}")
+
+printDetails(ID=101, Name="Laptop", Price=50000)
+print()
+printDetails(ID=102, Name="Mobile")
+print()
+printDetails(ID=103)  # Only ID provided, others will be default "NA"
